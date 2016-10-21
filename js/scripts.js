07e152c1d083caa941667587b1240ca1a83c7fec
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-10-21 14:31:09 -0700

    make proper tags in js files to add to radio

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,69 @@
 $(document).ready(function() {
-  $("form#survey").submit(function(event) {
-    debugger;
-    var yes1 = ($("input:radio[name=yes1]:checked").val());
-    var no1 = ($("input:radio[name=no]:checked").val());
-    var yes2 = ($("input:radio[name=yes2]:checked").val());
-    var no2 = ($("input:radio[name=no]:checked").val());
-    var yes3 = ($("input:radio[name=yes3]:checked").val());
-    var no3 = ($("input:radio[name=no]:checked").val());
-    var yes4 = ($("input:radio[name=yes4]:checked").val());
-    var no4 = ($("input:radio[name=no]:checked").val());
-    var yes5 = ($("input:radio[name=yes5]:checked").val());
-    var no5 = ($("input:radio[name=no]:checked").val());
+  $("form#questions").submit(function() {
+    var question1 = $("input:radio[name=yes1]:checked").val();
+    var question1 = $("input:radio[name=no1]:checked").val();
+    var question1 = $("input:radio[name=both1]:checked").val();
+    var question2 = $("input:radio[name=yes2]:checked").val();
+    var question2 = $("input:radio[name=no2]:checked").val();
+    var question2 = $("input:radio[name=both2]:checked").val();
+    var question3 = $("input:radio[name=yes3]:checked").val();
+    var question3 = $("input:radio[name=no3]:checked").val();
+    var question3 = $("input:radio[name=both3]:checked").val();
+    var question4 = $("input:radio[name=yes4]:checked").val();
+    var question4 = $("input:radio[name=no4]:checked").val();
+    var question4 = $("input:radio[name=both4]:checked").val();
+    var question5 = $("input:radio[name=yes5]:checked").val();
+    var question5 = $("input:radio[name=no5]:checked").val();
+    var question5 = $("input:radio[name=both5]:checked").val();
 
 
     console.log(yes)
     console.log(no)
 
-    if (yes1 === "yes1") {
+    if (question1 === "yes1") {
       $('#yes').show();
-    } else if (yes2 === "yes2") {
-      $('#yes').show();
-    } else if (yes3 === "yes3") {
-      $('#yes').show();
-    } else if (yes4 === "yes4") {
-      $('#yes').show();
-    } else if (yes5 === "yes5") {
-      $('#yes').show();
+    } else if (question1 === "no1") {
+      $('#no').show();
+    } else if (question1 === "both1") {
+      $('#both').show();
     }
+
+
+    if (question2 === "ye2") {
+      $('#yes').show();
+    } else if (question2 === "no2") {
+      $('#no').show();
+    } else if (question2 === "both2") {
+      $('#both').show();
+    }
+
+    if (question3 === "yes3") {
+      $('#yes').show();
+    } else if (question3 === "no3") {
+      $('#no').show();
+    } else if (question3 === "both3") {
+      $('#both').show();
+    }
+
+    if (question4 === "yes4") {
+      $('#yes').show();
+    } else if (question4 === "no4") {
+      $('#no').show();
+    } else if (question4 === "both4") {
+      $('#both').show();
+    }
+
+    if (question5 === "yes5" ) {
+      $('#yes').show();
+    } else if (question5 === "no5") {
+      $('#no').show();
+    } else if (question5 === "both5") {
+      $('#both').show();
+    }
+
+    $("#output").text(result);
+
+
 
     event.preventDefault();
   });
