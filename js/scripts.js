9f2111d545cadbf53ab1994386f2c79775964816
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-10-21 13:36:20 -0700

    add javascript id's for radio's

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     debugger;
-    var question1 = $("input:radio[name=yes].checked").val();
-    var question2 = $("input:radio[name=no]:checked").val();
+    var question1 = ($("input:radio[name=yes]:checked").val());
+    var question2 = ($("input:radio[name=no]:checked").val());
 
     console.log(yes)
     console.log(no)
 
     if (question1 === "yes") {
-      $('#cssDesign').show();
+      $('#yes').show();
     } else if (question2 === "yes") {
-      $('#cssDesign').show();
+      $('#yes').show();
     } else if (question3 === "yes") {
-      $('#cssDesign').show();
+      $('#yes').show();
     } else if (question4 === "yes") {
-      $('#cssDesign').show();
+      $('#yes').show();
     } else if (question5 === "yes") {
-      $('#cssDesign').show();
+      $('#yes').show();
     }
 
-event.preventDefault();
+    event.preventDefault();
   });
 });
