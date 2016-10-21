037e0ae35e0d022e78e16c1039f8cebfa0fa5c4c
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-10-21 15:09:49 -0700

    add classes to output tags for display when answers are set

--- js/scripts.js
+++ js/scripts.js
@@ -4,19 +4,22 @@ $(document).ready(function() {
     var question1 = $("input:radio[name=yes1]:checked").val();
     var question1 = $("input:radio[name=no1]:checked").val();
     var question1 = $("input:radio[name=both1]:checked").val();
+
     var question2 = $("input:radio[name=yes2]:checked").val();
     var question2 = $("input:radio[name=no2]:checked").val();
     var question2 = $("input:radio[name=both2]:checked").val();
+
     var question3 = $("input:radio[name=yes3]:checked").val();
     var question3 = $("input:radio[name=no3]:checked").val();
     var question3 = $("input:radio[name=both3]:checked").val();
+
     var question4 = $("input:radio[name=yes4]:checked").val();
     var question4 = $("input:radio[name=no4]:checked").val();
     var question4 = $("input:radio[name=both4]:checked").val();
+
     var question5 = $("input:radio[name=yes5]:checked").val();
     var question5 = $("input:radio[name=no5]:checked").val();
     var question5 = $("input:radio[name=both5]:checked").val();
-
 
     console.log(yes)
     console.log(no)
@@ -24,41 +27,41 @@ $(document).ready(function() {
     if (question1 === "yes1" || question1 != "no1" || question1 != "both1") {
       $('#cssDesign').show();
     } else if (question1 === "no1" || question1 != "yes1" || question1 != "both1") {
-      $('#no').show();
+      $('#cnet').show();
     } else if (question1 === "both1" || question1 != "yes1" || question1 != "no1") {
-      $('#both').show();
+      $('#javaAndroid').show();
     }
 
     if (question2 === "yes2" || question2 != "no2" || question2 != "both2") {
       $('#cssDesign').show();
     } else if (question2 === "no2" || question2 != "yes2" || question2 != "both1") {
-      $('#no').show();
+      $('#cnet').show();
     } else if (question2 === "both2" || question2 != "yes2" || question2 != "no2") {
-      $('#both').show();
+      $('#javaAndroid').show();
     }
 
     if (question3 === "yes3" || question3 != "no3" || question3 != "both3") {
       $('#cssDesign').show();
     } else if (question3 === "no3" || question3 != "yes3" || question3 != "both3") {
-      $('#no').show();
+      $('#cnet').show();
     } else if (question3 === "both3" || question3 != "yes3" || question3 != "no3") {
-      $('#both').show();
+      $('#javaAndroid').show();
     }
 
     if (question4 === "yes4" || question4 != "no4" || question4 != "both4") {
       $('#cssDesign').show();
     } else if (question4 === "no4" || question4 != "yes4" || question4 != "both4") {
-      $('#no').show();
+      $('#cnet').show();
     } else if (question4 === "both4" || question4 != "yes4" || question4 != "no1") {
-      $('#both').show();
+      $('#javaAndroid').show();
     }
 
     if (question5 === "yes5" || question5 != "no5" || question5 != "both5")
       $('#cssDesign').show();
     } else if (question5 === "no5" || question5 != "yes5" || question5 != "both5") {
-      $('#no').show();
+      $('#cnet').show();
     } else if (question5 === "both5" || question5 != "yes5" || question5 != "no5") {
-      $('#both').show();
+      $('#javaAndroid').show();
     }
 
     $("#output").text(result);
