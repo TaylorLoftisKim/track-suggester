f110a15bcc0a4ce3702c6399ad1d265ece2e17e4
Taylor Loftis-Kim <taylorloftiskim@gmail.com>
2016-10-21 13:51:48 -0700

    change id tags and checked tags in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     debugger;
-    var question1 = ($("input:radio[name=yes]:checked").val());
-    var question2 = ($("input:radio[name=no]:checked").val());
+    var yes1 = ($("input:radio[name=yes1]:checked").val());
+    var no1 = ($("input:radio[name=no]:checked").val());
+    var yes2 = ($("input:radio[name=yes2]:checked").val());
+    var no2 = ($("input:radio[name=no]:checked").val());
+    var yes3 = ($("input:radio[name=yes3]:checked").val());
+    var no3 = ($("input:radio[name=no]:checked").val());
+    var yes4 = ($("input:radio[name=yes4]:checked").val());
+    var no4 = ($("input:radio[name=no]:checked").val());
+    var yes5 = ($("input:radio[name=yes5]:checked").val());
+    var no5 = ($("input:radio[name=no]:checked").val());
+
 
     console.log(yes)
     console.log(no)
 
-    if (question1 === "yes") {
+    if (yes1 === "yes1") {
       $('#yes').show();
-    } else if (question2 === "yes") {
+    } else if (yes2 === "yes2") {
       $('#yes').show();
-    } else if (question3 === "yes") {
+    } else if (yes3 === "yes3") {
       $('#yes').show();
-    } else if (question4 === "yes") {
+    } else if (yes4 === "yes4") {
       $('#yes').show();
-    } else if (question5 === "yes") {
+    } else if (yes5 === "yes5") {
       $('#yes').show();
     }
 
